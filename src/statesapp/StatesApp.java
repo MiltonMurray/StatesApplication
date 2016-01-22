7185910d9e35c8b6ad66b3d8106ba1b80b323d14
Milton Murray <miltonmurray@outlook.com>
2016-01-21 21:09:01 -0500

    Initial Project Version

---
+++ src/statesapp/StatesApp.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package statesapp;
+
+/**
+ *
+ * @author milto_000
+ */
+public class StatesApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
