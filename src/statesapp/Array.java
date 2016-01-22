da22c04f734c824d8c447507f391086c3d75950c
Milton Murray <miltonmurray@outlook.com>
2016-01-21 21:25:23 -0500

    Added design

--- src/statesapp/Array.java
+++ src/statesapp/Array.java
@@ -11,5 +11,20 @@ package statesapp;
  * @author milto_000
  */
 public class Array {
-    
+    public static void loadArray(States[] usa)
+    {
+        // a loop counter
+        
+        // declare temporary variables to hold
+        
+        // Create a File class object linked to the name of the file to be read
+        
+        // Create a Scanner named infile to read the input stream from the file
+        
+        //loop to read data in the array
+        
+            //read in temporary variables
+        
+        //initialize each variable with the States contructor
+    }
 }
--- src/statesapp/StatesApp.java
+++ src/statesapp/StatesApp.java
@@ -3,20 +3,34 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package statesapp;
 
-/**
+/*******************************************************************************
+ * Create a software package as a NetBeans project that contains three classes:
+ *      > an executable class for the project itself 
+ *      > a class of states, with the properties name, capital, and population
+ *      > a class for an array of state objects
+ * 
+ * The state and array classes should contain appropriate properties and methods
+ * to work with those properties. 
+ * Your project’s class (with the same name as the package) should contain a main
+ * method, and methods to test your other classes as follows:
+ *      > load the data from the data file into the array of states. 
+ *      > print the list of states -- including each state’s name, capital and
+ *        population -- with the data for each state on a separate line. 
+ *      > a method that asks the user for the name of a state, then either 
+ *        displays the data for that state, or says the
+ *        state was not found in the list.
+ *
+ * The main method should simply call the other methods in the class to show
+ * that they work.
  *
  * @author milto_000
- */
+ ******************************************************************************/
 public class StatesApp {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
